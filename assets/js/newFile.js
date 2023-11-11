b4610f923605e8393094f2a3f8c489ce13070a78
Samarium02 <92641801+Samarium02@users.noreply.github.com>
2023-11-12 03:21:53 +0530

    compeleted project on 10-11-23

---
+++ assets/js/newFile.js
@@ -0,0 +1,10 @@
+sr.reveal(
+  ".home__name, .home__info, ".about__container.section__title - 1,
+  about__info,
+  contact__social,
+  contact__data,
+  ", {origin: ",
+  left,
+  "})",
+  sr.reveal(".services__card .projects__card", { origin: "right" })
+);
